9c026025411905e4fd0da037e9757a2b59663d30
Tinu Clara Emmanuel <tinu.clara@geekywolf.com>
2025-10-23 13:32:33 +0530

    Day5 C#

---
+++ Dotnet/Day5/MathUtilities/MathUtilityImplementation/Program.cs
@@ -0,0 +1,9 @@
+﻿using MathUtilities;
+
+Console.WriteLine("Enter the number");
+int num = int.Parse(Console.ReadLine());
+
+MathUtilities.MathUtilities mathUtilities = new MathUtilities.MathUtilities();
+Console.WriteLine(mathUtilities.IsEven(num));
+Console.WriteLine(mathUtilities.IsPrime(num));
+Console.WriteLine(mathUtilities.Factorial(num));   
