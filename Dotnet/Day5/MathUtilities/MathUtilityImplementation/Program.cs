b630223cdbbb7969d9c7e6e1646370d8d7dc639b
Tinu Clara Emmanuel <tinu.clara@geekywolf.com>
2025-10-23 13:29:48 +0530

    deleted day5

--- Dotnet/Day5/MathUtilities/MathUtilityImplementation/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using MathUtilities;
-
-Console.WriteLine("Enter the number");
-int num = int.Parse(Console.ReadLine());
-
-MathUtilities.MathUtilities mathUtilities = new MathUtilities.MathUtilities();
-Console.WriteLine(mathUtilities.IsEven(num));
-Console.WriteLine(mathUtilities.IsPrime(num));
-Console.WriteLine(mathUtilities.Factorial(num));   
--- Dotnet/Day5/ReportsCompanyAAndB/ReportsCompanyAAndB/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using CompanyA.Reporting;
-using CompanyB.Analytics;
-
-CompanyA.Reporting.Report report = new CompanyA.Reporting.Report();
-CompanyB.Analytics.Report report1=new CompanyB.Analytics.Report();
-
-report.Generate();
-report1.Generate();
-
