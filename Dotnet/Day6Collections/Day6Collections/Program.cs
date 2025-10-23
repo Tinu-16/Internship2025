113d3e4c047f580f0d9eb8d36fdf7a92a5b2a02c
Tinu Clara Emmanuel <tinu.clara@geekywolf.com>
2025-10-23 13:43:53 +0530

    Day6 C#

---
+++ Dotnet/Day6Collections/Day6Collections/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using Day6Collections;
+
+Question1.Operations();
+Question2.Operations();
+Question3.Operations();
+Question4.Operations();
+Question5.Operations();
+
+
+Console.WriteLine("-----------------------Question6--------------------------");
+Console.WriteLine("Welcome to the Employee Management System…\r\n");
+EmployeeCollection collection = new EmployeeCollection();
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Please choose one of the following:\r\n");
+    Console.WriteLine("\n1.Add Employee\r\n2.Remove Employee\r\n3.Display All Employees\r\n4.Search Employee\r\n5.Exit\r\n");
+    int choice=int.Parse(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("Enter the name");
+            string name=Console.ReadLine();
+
+            Console.WriteLine("Enter the salary");
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Employee type : ");
+            string type = Console.ReadLine();
+
+            collection.AddEmployee(name, salary, type);
+            break;
+        case 2:
+            Console.WriteLine("Enter the Id of the employee: ");
+            string id = Console.ReadLine();
+
+            collection.RemoveEmployee(id);
+            break;
+        case 3:
+            collection.DisplayEmployees();
+            break;
+        case 4:
+            Console.WriteLine("Enter the name of the employee: ");
+            string name1 = Console.ReadLine();
+
+            collection.SearchEmployee(name1);
+            break;
+        case 5:
+            break;
+        default:
+            Console.WriteLine("Choose a valid option");
+            break;
+    }
+    if (choice == 5)
+        break;
+}
---
+++ Dotnet/Day6Collections/Day6Collections/Question5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Collections
+{
+    internal class Question5
+    {
+        public static void Operations()
+        {
+            Console.WriteLine("-----------------------Question5--------------------------");
+            //Create a List<string> of 6 names.
+            List<string> names = new List<string>();
+            names.Add("Anu");
+            names.Add("Minu");
+            names.Add("Tinu");
+            names.Add("Sophy");
+            names.Add("Serah");
+            names.Add("anugraha");
+
+            //Find all names that start with 'A'.
+            var namesWithA = names.Where(x => x.StartsWith("A", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("Names starting with 'A'");
+            foreach (var name in namesWithA)
+            {
+                Console.WriteLine(name);
+            }
+
+            //Find names with length greater than 4.
+            Console.WriteLine("Names whose length is greater than 4");
+            var lengthGreaterThanFour = names.Where(x => x.Length > 4);
+            foreach (var name in lengthGreaterThanFour)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
