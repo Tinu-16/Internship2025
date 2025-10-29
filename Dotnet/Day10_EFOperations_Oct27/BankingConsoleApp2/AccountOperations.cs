ec12396236d8d74fbdb1ed3f81a013446d03b5f1
Tinu Clara Emmanuel <tinu.clara@geekywolf.com>
2025-10-29 18:52:15 +0530

    Day10 C# Oct27

---
+++ Dotnet/Day10_EFOperations_Oct27/BankingConsoleApp2/AccountOperations.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankingConsoleApp2
+{
+    public class AccountOperations
+    {
+        private readonly BankingAppDbContext _context;
+        public AccountOperations(BankingAppDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Customer customer)
+        {
+            try
+            {
+                _context.Add(customer);
+                int result = _context.SaveChanges();
+
+                if (result > 0)
+                {
+                    Console.WriteLine("Customer inserted successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Customer not inserted successfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Display()
+        {
+            List<Customer> customers = _context.Customers
+                                       .Include(c => c.Address)
+                                       .Include(c => c.Account)
+                                       .ToList();
+            if (customers.Count > 0)
+            {
+                foreach (Customer customer in customers)
+                {
+                    string accountsDetail = string.Join(", ", customer.Account.Select(a =>
+                    $"{a.AccountNumber} ({a.Balance})"));
+                    Console.WriteLine($"{customer.FullName} : <>; Address : <{customer.Address.Street},{customer.Address.City} ,{customer.Address.State} ,{customer.Address.PostalCode} ,{customer.Address.Country}>; Account : <{accountsDetail}>");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("The customer table is empty");
+            }
+        }
+
+        public void AddAddress(int customerId, Address newAddress)
+        {
+            try
+            {
+                var customer = _context.Customers.Include(c => c.Address).FirstOrDefault(c => c.Id == customerId);
+
+                if (customer.Address == null)
+                {
+                    newAddress.CustomerId = customerId;
+                    _context.Addresses.Add(newAddress);
+                }
+                else
+                {
+                    customer.Address.Street = newAddress.Street;
+                    customer.Address.City = newAddress.City;
+                    customer.Address.State = newAddress.State;
+                    customer.Address.PostalCode = newAddress.PostalCode;
+                    customer.Address.Country = newAddress.Country;
+                }
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void AddAccount(int customerId, Account account)
+        {
+            try
+            {
+                var customer = _context.Customers.FirstOrDefault(a => a.Id == customerId);
+                if (customer == null)
+                {
+                    Console.WriteLine($"Customer with customer id : {customerId} does not exists");
+                }
+                else
+                {
+                    account.CustomerId = customerId;
+                    _context.Accounts.Add(account);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void DeleteAccount(int customerId, int accountId)
+        {
+            try
+            {
+                var account = _context.Accounts.FirstOrDefault(a => a.Id == accountId && a.CustomerId== customerId);
+                if (account == null)
+                {
+                    Console.WriteLine($"Account id does not exist");
+                }
+                else
+                {
+                    _context.Accounts.Remove(account);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+
+    }
+}
---
+++ Dotnet/Day10_EFOperations_Oct27/BankingConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using BankingConsoleApp2;
+
+using (var context = new BankingAppDbContext())
+{
+    AccountOperations accountOperations = new AccountOperations(context);
+
+    var customer = new Customer
+    {
+        FullName = "Tinu Clara",
+        Email = "tinu@gmail.com",
+        PhoneNumber = "9856959555",
+        DateOfBirth = new DateTime(2020, 07, 10),
+        Address = new Address
+        {
+            Street = "Paloorkavu C.P.O",
+            City = "Mundakayam",
+            State = "Kerala",
+            PostalCode = "685532",
+            Country = "India"
+        },
+        Account = new List<Account>()
+    };
+
+    var account = new Account
+    {
+        AccountNumber = "555555",
+        Balance = 3450.99,
+    };
+    customer.Account.Add(account);
+    accountOperations.Add(customer);
+
+    Console.WriteLine("----------Display------------");
+    accountOperations.Display();
+
+    Console.WriteLine("Enter the customer id");
+    int id = int.Parse(Console.ReadLine());
+
+    Address address1 = new Address();
+    Console.WriteLine("Enter the street");
+    address1.Street = Console.ReadLine();
+    Console.WriteLine("Enter the city");
+    address1.City = Console.ReadLine();
+    Console.WriteLine("Enter the state");
+    address1.State = Console.ReadLine();
+    Console.WriteLine("Enter the postal code");
+    address1.PostalCode = Console.ReadLine();
+    Console.WriteLine("Enter the country");
+    address1.Country = Console.ReadLine();
+    accountOperations.AddAddress(id, address1);
+
+    Account account1 = new Account();
+    Console.WriteLine("Enter the customer id");
+    int customerId = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter the account no");
+    account1.AccountNumber = Console.ReadLine();
+    accountOperations.AddAccount(customerId, account1);
+
+    Console.WriteLine("Enter the customer id");
+    int customersId = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter the account id");
+    int accountId = int.Parse(Console.ReadLine());
+    accountOperations.DeleteAccount(customerId, accountId);
+}
